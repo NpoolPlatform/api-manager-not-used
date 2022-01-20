5d3ce2ba6ba54a505f0b5ee592ddd567e85c73f4
Zhao KK <kikakkz@hotmail.com>
2022-01-20 15:35:25 +0000

    Correct regex

--- pkg/crud/service-api/serviceapi.go
+++ pkg/crud/service-api/serviceapi.go
@@ -7,6 +7,7 @@ import (
 	"github.com/NpoolPlatform/api-manager/pkg/db"
 	npool "github.com/NpoolPlatform/message/npool/apimgr"
 
+	"github.com/google/uuid"
 	"golang.org/x/xerrors"
 )
 
@@ -57,6 +58,52 @@ func Register(ctx context.Context, in *npool.RegisterRequest) (*npool.RegisterRe
 	return &npool.RegisterResponse{
 		Info: in.GetInfo(),
 	}, nil
+}
+
+func Update(ctx context.Context, apis []*npool.ServicePath) error {
+	ctx, cancel := context.WithTimeout(ctx, grpcTimeout)
+	defer cancel()
+
+	cli, err := db.Client()
+	if err != nil {
+		return xerrors.Errorf("fail get db client: %v", err)
+	}
+
+	tx, err := cli.Tx(ctx)
+	if err != nil {
+		return xerrors.Errorf("fail create transaction: %v", err)
+	}
+
+	for _, api := range apis {
+		id, err := uuid.Parse(api.ID)
+		if err != nil {
+			if rerr := tx.Rollback(); rerr != nil {
+				return xerrors.Errorf("fail rollback update service api: %v (%v)", rerr, err)
+			}
+			return xerrors.Errorf("fail parse service api id: %v", err)
+		}
+
+		_, err = tx.
+			ServiceAPI.
+			UpdateOneID(id).
+			SetPathPrefix(api.PathPrefix).
+			SetExported(api.Exported).
+			SetDomains(api.Domains).
+			Save(ctx)
+		if err != nil {
+			if rerr := tx.Rollback(); rerr != nil {
+				return xerrors.Errorf("fail rollback update service api: %v (%v)", rerr, err)
+			}
+			return xerrors.Errorf("fail update service api: %v", err)
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return xerrors.Errorf("fail commit service api update: %v", err)
+	}
+
+	return nil
 }
 
 func GetApis(ctx context.Context, in *npool.GetApisRequest) (*npool.GetApisResponse, error) {
--- pkg/middleware/service-api/serviceapi.go
+++ pkg/middleware/service-api/serviceapi.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"regexp"
+	"strings"
 	"time"
 
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
@@ -21,8 +23,53 @@ type EntryPoint struct {
 
 var notifier = make(chan struct{})
 
-func updater() {
+func (ep EntryPoint) Domain() string {
+	// "Host(`api.xpool.top`) \u0026\u0026 PathPrefix(`/api/cloud-hashing-apis/version`)"
+	regex := regexp.MustCompile(`(?:([a-z0-9-]+|\*)\.)?([a-z0-9-]{1,61})\.([a-z0-9]{2,7})`)
+	domain := regex.Find([]byte(ep.Rule))
+	return string(domain)
+}
+
+func (ep EntryPoint) ExportedPath() string {
+	regex := regexp.MustCompile(`/[a-z0-9-/]+`)
+	path := regex.Find([]byte(ep.Rule))
+	return string(path)
+}
+
+func (ep EntryPoint) PathPrefix() (string, error) {
+	path := ep.ExportedPath()
+	paths := strings.Split(path, "/")
+	if len(paths) < 3 {
+		return "", xerrors.Errorf("invalid exported path")
+	}
+
+	return strings.Join(paths[0:3], "/"), nil
+}
+
+func (ep EntryPoint) ServiceKey() (string, error) {
+	path := ep.ExportedPath()
+	paths := strings.Split(path, "/")
+	if len(paths) < 3 {
+		return "", xerrors.Errorf("invalid exported path")
+	}
+
+	return paths[2], nil
+}
+
+func (ep EntryPoint) Path() (string, error) {
+	path := ep.ExportedPath()
+	paths := strings.Split(path, "/")
+	if len(paths) < 3 {
+		return "", xerrors.Errorf("invalid exported path")
+	}
+
+	return fmt.Sprintf("/%v", strings.Join(paths[3:], "/")), nil
+}
+
+func updater() { //nolint
 	pageIndex := 1
+	allRouters := []EntryPoint{}
+
 	for {
 		url := fmt.Sprintf("http://traefik.kube-system.svc.cluster.local:38080/api/http/routers?page=%v", pageIndex)
 		resp, err := resty.New().R().Get(url)
@@ -35,18 +82,66 @@ func updater() {
 		err = json.Unmarshal(resp.Body(), &routers)
 		if err != nil {
 			logger.Sugar().Errorf("fail parse body: %v", err)
-			return
-		}
-
-		for _, router := range routers {
-			logger.Sugar().Info(router)
+			break
 		}
 
 		if len(routers) == 0 {
 			break
 		}
 
+		allRouters = append(allRouters, routers...)
 		pageIndex++
+	}
+
+	resp, err := crud.GetApis(context.Background(), &npool.GetApisRequest{})
+	if err != nil {
+		logger.Sugar().Errorf("fail get apis: %v", err)
+		return
+	}
+
+	for _, router := range allRouters {
+		domain := router.Domain()
+		prefix, err := router.PathPrefix()
+		if err != nil {
+			logger.Sugar().Errorf("fail get prefix of %v: %v", router, err)
+			continue
+		}
+		path, err := router.Path()
+		if err != nil {
+			logger.Sugar().Errorf("fail get path of %v: %v", router, err)
+			continue
+		}
+		serviceKey, err := router.ServiceKey()
+		if err != nil {
+			logger.Sugar().Errorf("fail get service key of %v: %v", router, err)
+			continue
+		}
+
+		for _, info := range resp.Infos {
+			if !strings.Contains(info.ServiceName, serviceKey) || !strings.Contains(info.Path, path) {
+				continue
+			}
+
+			info.PathPrefix = prefix
+			info.Exported = true
+
+			contains := false
+			for _, dom := range info.Domains {
+				if dom == domain {
+					contains = true
+					break
+				}
+			}
+
+			if !contains {
+				info.Domains = append(info.Domains, domain)
+			}
+		}
+	}
+
+	err = crud.Update(context.Background(), resp.Infos)
+	if err != nil {
+		logger.Sugar().Errorf("fail update apis: %v", err)
 	}
 }
 
